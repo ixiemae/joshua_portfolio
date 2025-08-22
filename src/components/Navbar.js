89c994fb04eeca4e0b6505ea92bd6da3231c87e4
Trixie <trixiemotil@gmail.com>
2025-08-22 12:22:48 +0800

    updated something in my project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,43 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/Navbar.css'; 
 
 const Navbar = () => {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const handleHamburgerClick = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
           MyPortfolio
         </Link>
-        <ul className="nav-menu">
+        <div className="hamburger" onClick={handleHamburgerClick}>
+          <span />
+          <span />
+          <span />
+        </div>
+        <ul className={`nav-menu${menuActive ? ' active' : ''}`}>
           <li className="nav-item">
-            <Link to="/" className="nav-links">
+            <Link to="/" className="nav-links" onClick={() => setMenuActive(false)}>
               Home
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/about" className="nav-links">
+            <Link to="/about" className="nav-links" onClick={() => setMenuActive(false)}>
               About
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/projects" className="nav-links">
+            <Link to="/projects" className="nav-links" onClick={() => setMenuActive(false)}>
               Projects
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/contact" className="nav-links">
+            <Link to="/contact" className="nav-links" onClick={() => setMenuActive(false)}>
               Contact
             </Link>
           </li>
